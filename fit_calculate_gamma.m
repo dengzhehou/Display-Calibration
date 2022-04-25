72e6aa9c57b682a0664e8ed57634a9b91d1e85f6
dzhou <houdengzhe@gmail.com>
2022-04-25 15:10:20 +0900

    Add files via upload

--- fit_calculate_gamma.m
+++ fit_calculate_gamma.m
@@ -1,3 +1,5 @@
+opengl software
+
 ds = readtable('display_calibration_data.xlsx', 'sheet','data');
 
 voltage = table2array(ds(:, 1));
@@ -12,9 +14,8 @@ fittedmodel = fit(voltage,luminance,g);
 displayGamma = fittedmodel.g;
 gammaTable1 = ((([0:3:maxLevel]'/maxLevel))).^(1/fittedmodel.g); %#ok<NBRAK>
 
-plot(voltage, luminance, '.')
-hold on
-plot(voltage, voltage.^2.39)
-% plot(voltage, voltage.^(1/2.39))
-plot(voltage, luminance.^(1/2.39))
-hold off
+
+plot(voltage,luminance,'.b', voltage,voltage.^displayGamma,'g', voltage,luminance.^(1/displayGamma),'r')
+xlabel('voltage')
+ylabel('luminance')
+legend('collected luminance','predicted luminance','corrected luminance', 'Location','northwest')
